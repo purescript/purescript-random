5ef6cceea4983fe374621963622c8b9e1e90a4c6
Gary Burgess <gary.burgess@gmail.com>
2014-03-19 21:41:19 +0000

    Initial commit

---
+++ src/Control/Monad/Eff/Random.purs
@@ -0,0 +1,10 @@
+module Control.Monad.Eff.Random where
+
+import Control.Monad.Eff
+
+foreign import data Random :: !
+
+foreign import random
+  "function random() {\
+  \  return Math.random();\
+  \}" :: forall e. Eff (random :: Random | e) Number
