b03857ea211714f0354bc60eb42debd6e1d63d7c
Gary Burgess <gary.burgess@gmail.com>
2014-04-04 23:41:41 +0100

    Revise build, add `choice` extra

---
+++ src/Control/Monad/Eff/Random/Extras.purs
@@ -0,0 +1,12 @@
+module Control.Monad.Eff.Random.Extras where
+
+import Control.Monad.Eff
+import Control.Monad.Eff.Random
+import Data.Array
+import Data.Maybe
+
+choice :: forall a e. [a] -> Eff (random :: Random | e) (Maybe a)
+choice [] = return Nothing
+choice xs = do
+  n <- random
+  return $ Just $ xs !! Math.floor (length xs * n)
