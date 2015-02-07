61a1400c28ad7e3953e9f174e0d7b32a2a4836d9
Gary Burgess <gary.burgess@gmail.com>
2015-02-07 12:29:55 +0000

    Cosmetic tweaks

--- README.md
+++ README.md
@@ -10,14 +10,16 @@
 
 ### Values
 
+     | Returns a random number between 0 (inclusive) and 1 (exclusive).
 
     random :: forall e. Eff (random :: Random | e) Number
 
-     Takes a range _low_ _high_ and returns a random int uniformly distributed in the
-     closed interval [lo,hi]. It is unspecified what happens if lo>hi.
+     | Takes a range `low` `high` and returns a random int uniformly distributed
+     | in the closed interval `[low, high]`. It is unspecified what happens if
+     | `low > high`.
 
     randomInt :: forall e. Number -> Number -> Eff (random :: Random | e) Number
 
-     Returns a random number between min (inclusive) and max (exclusive)
+     | Returns a random number between min (inclusive) and max (exclusive).
 
     randomRange :: forall e. Number -> Number -> Eff (random :: Random | e) Number
--- src/Control/Monad/Eff/Random.purs
+++ src/Control/Monad/Eff/Random.purs
@@ -1,24 +1,28 @@
 module Control.Monad.Eff.Random where
 
 import Control.Monad.Eff
-import Math(floor)
+import Math (floor)
 
 foreign import data Random :: !
 
+-- | Returns a random number between 0 (inclusive) and 1 (exclusive).
 foreign import random
-  "function random() {\
-  \  return Math.random();\
-  \}" :: forall e. Eff (random :: Random | e) Number
+  """
+  function random() {
+    return Math.random();
+  }
+  """ :: forall e. Eff (random :: Random | e) Number
 
--- Takes a range _low_ _high_ and returns a random int uniformly distributed in the
--- closed interval [lo,hi]. It is unspecified what happens if lo>hi.
+-- | Takes a range `low` `high` and returns a random int uniformly distributed
+-- | in the closed interval `[low, high]`. It is unspecified what happens if
+-- | `low > high`.
 randomInt :: forall e . Number -> Number -> Eff (random :: Random | e) Number
 randomInt low high = do
-    rand <- random
-    return (floor (rand * (high - low + 1)) + low)
+  rand <- random
+  return (floor (rand * (high - low + 1)) + low)
 
--- Returns a random number between min (inclusive) and max (exclusive)
+-- | Returns a random number between min (inclusive) and max (exclusive).
 randomRange :: forall e . Number -> Number -> Eff (random :: Random | e) Number
 randomRange min max = do
-    r <- random
-    return (r * (max - min) + min)
+  r <- random
+  return (r * (max - min) + min)
